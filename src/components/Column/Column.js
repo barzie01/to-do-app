990c6aa7a0a527b3c21bbbd757e0cb3d8d79c522
Bartosz Ziętak <bartekzietak01@gmail.com>
2021-07-08 17:51:15 +0200

    Add state do List component. Use data from dataStore.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 
 class Column extends React.Component {
   static propTypes = {
-    colTitle: PropTypes.string,
+    columnProps: PropTypes.string,
   }
 
   render() {
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.colTitle}</h3>
+        <h3 className={styles.title}>{this.props.title}</h3>
       </section>
         )
     }
