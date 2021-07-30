e92a974f0f7b4db6aa472084580b63487d7e62be
Bartosz Ziętak <bartekzietak01@gmail.com>
2021-07-30 22:43:04 +0200

    Fix problem with implementing Header inside MainLayout component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,29 @@
-// import React from 'react';
-// import {NavLink, Link} from 'react-router-dom';
-// import styles from './Header.scss';
+import React from 'react';
+import {NavLink, Link} from 'react-router-dom';
+import Container from '../Container/Container';
+import styles from './Header.scss';
+import Icon from '../Icon/Icon';
+
+class Header extends React.Component {
+
+  render () {
+   
+    return (
+      <Header className={styles.component}>
+        <Container>
+          <div className={styles.wrapper}>
+            <Link to='/' className={styles.logo}>
+              <Icon name='rocket' />
+            </Link>
+            <nav>
+              <NavLink exact to='/'>Home</NavLink>
+              <NavLink exact to='/info'>Info</NavLink>
+            </nav>
+          </div>
+        </Container>
+      </Header>
+    );
+  }
+}
+
+export default Header;
